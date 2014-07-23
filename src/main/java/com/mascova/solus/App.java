eef86abbcf8ebf3ed975c2be539328f7088558f5
Irfan Romadona <irfan.romadona@gmail.com>
2014-07-24 00:12:27 +0700

    initial commit

---
+++ src/main/java/com/mascova/solus/App.java
@@ -0,0 +1,13 @@
+package com.mascova.solus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
